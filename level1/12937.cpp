3376e49acd77955028ab35fa86d250d94cea1d4b
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2019-08-28 15:15:56 +0900

    Create 12937.cpp

---
+++ level1/12937.cpp
@@ -0,0 +1,21 @@
+//짝수와_홀수
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    if(num % 2 == 0)
+    {
+        answer.append("Even");
+    }
+    else
+    {
+        answer.append("Odd");
+    }
+    
+    return answer;
+}
