9bd8b20e7bf6344ee6066d000c3b77d97b40ca22
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2019-08-28 15:25:46 +0900

    Create 12935.cpp

---
+++ level1/12935.cpp
@@ -0,0 +1,38 @@
+//제일_작은_수_제거하기
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    int min = 2100000000;
+    int mdx;
+    
+    if(arr.size() == 1)
+    {
+        answer.push_back(-1);
+    }
+    else
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i];
+                mdx = i;
+            }
+        }
+        
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(i != mdx)
+            {
+                answer.push_back(arr[i]);
+            }
+        }
+    }
+    
+    return answer;
+}
