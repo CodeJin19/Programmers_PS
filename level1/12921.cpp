d81c80508c4cf062c9cc96ea7ae4cd38306eeb99
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2019-09-02 15:24:37 +0900

    Create 12921.cpp

---
+++ level1/12921.cpp
@@ -0,0 +1,38 @@
+//소수_찾기
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 1;
+    int x;
+    bool arr[1000010];
+    
+    for(int i = 3; i <= n; i++)
+    {
+        arr[i] = true;
+    }
+    
+    for(int i = 3; i <= n;)
+    {
+        x = 0;
+        
+        if(arr[i])
+        {
+            answer++;
+            
+            while(x <= n && x <= (1000010 - i))
+            {
+                x += i;
+                arr[x] = false;
+            }
+        }
+        
+        i += 2;
+    }
+    
+    return answer;
+}
