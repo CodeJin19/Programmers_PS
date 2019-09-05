f67b327cd644bce5aaf0d73c68938f589c8ce317
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2019-09-05 14:43:34 +0900

    Create 12947.cpp

---
+++ level1/12947.cpp
@@ -0,0 +1,24 @@
+//하샤드_수
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(int x) {
+    bool answer = true;
+    int sum = 0;
+    int num = x;
+    
+    while(num != 0)
+    {
+        sum += (num % 10);
+        num /= 10;
+    }
+    
+    if(x % sum != 0)
+    {
+        answer = false;
+    }
+    return answer;
+}
