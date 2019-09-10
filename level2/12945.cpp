b0bd282ae06fd9c3ed5661729885a86add6233dc
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2019-09-10 13:50:39 +0900

    Create 12945.cpp

---
+++ level2/12945.cpp
@@ -0,0 +1,41 @@
+//피보나치_수
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    int a, b, c, cnt;
+    
+    if(n <= 1)
+    {
+        answer = n;
+    }
+    else
+    {
+        a = 0;
+        b = 1;
+        c = 1;
+        cnt = 2;
+        
+        while(cnt < n)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            
+            if(1234567 <= c)
+            {
+                c %= 1234567;
+            }
+            
+            cnt++;
+        }
+        
+        answer = c;
+    }
+    
+    return answer;
+}
