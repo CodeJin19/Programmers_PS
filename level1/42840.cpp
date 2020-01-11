241e9864d988b522c364089b14c01279d1a5b06f
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-01-11 16:41:19 +0900

    Update 42840.cpp

--- level1/42840.cpp
+++ level1/42840.cpp
@@ -7,8 +7,11 @@ using namespace std;
 
 vector<int> solution(vector<int> answers) {
     vector<int> answer;
+    int first[5] = {1, 2, 3, 4, 5};
+    int second[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int third[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
     int score[3];
-    int x, max;
+    int max = 0;
     
     for(int i = 0; i < 3; i++)
     {
@@ -16,125 +19,22 @@ vector<int> solution(vector<int> answers) {
     }
     
     for (int i = 0; i < answers.size(); i++)
-	{
-		x = i % 5;
-
-		switch (x)
-		{
-		case 0:
-			if (1 == answers[i])
-			{
-				score[0] += 1;
-			}
-			break;
-		case 1:
-			if (2 == answers[i])
-			{
-				score[0] += 1;
-			}
-			break;
-		case 2:
-			if (3 == answers[i])
-			{
-				score[0] += 1;
-			}
-			break;
-		case 3:
-			if (4 == answers[i])
-			{
-				score[0] += 1;
-			}
-			break;
-		case 4:
-			if (5 == answers[i])
-			{
-				score[0] += 1;
-			}
-			break;
-		}
-
-		x = i % 8;
-
-		switch (x)
-		{
-		case 0:
-		case 2:
-		case 4:
-		case 6:
-			if (2 == answers[i])
-			{
-				score[1] += 1;
-			}
-			break;
-		case 1:
-			if (1 == answers[i])
-			{
-				score[1] += 1;
-			}
-			break;
-		case 3:
-			if (3 == answers[i])
-			{
-				score[1] += 1;
-			}
-			break;
-		case 5:
-			if (4 == answers[i])
-			{
-				score[1] += 1;
-			}
-			break;
-		case 7:
-			if (5 == answers[i])
-			{
-				score[1] += 1;
-			}
-			break;
-		}
-
-		x = i % 10;
-
-		switch (x)
-		{
-		case 0:
-		case 1:
-			if (3 == answers[i])
-			{
-				score[2] += 1;
-			}
-			break;
-		case 2:
-		case 3:
-			if (1 == answers[i])
-			{
-				score[2] += 1;
-			}
-			break;
-		case 4:
-		case 5:
-			if (2 == answers[i])
-			{
-				score[2] += 1;
-			}
-			break;
-		case 6:
-		case 7:
-			if (4 == answers[i])
-			{
-				score[2] += 1;
-			}
-			break;
-		case 8:
-		case 9:
-			if (5 == answers[i])
-			{
-				score[2] += 1;
-			}
-			break;
-		}
+	{        
+        if(first[i % 5] == answers[i])
+        {
+            score[0] += 1;
+        }
+        
+        if(second[i % 8] == answers[i])
+        {
+            score[1] += 1;
+        }
+        
+        if(third[i % 10] == answers[i])
+        {
+            score[2] += 1;
+        }
 	}
-    
-    max = 0;
     
     for(int i = 0; i < 3; i++)
     {
