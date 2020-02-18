2480ee8b73e2051d3fd6109b20203d5bafe1a563
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-02-18 15:07:03 +0900

    Create 12987.cpp

---
+++ level2/12987.cpp
@@ -0,0 +1,42 @@
+//숫자 게임
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = 0;
+    int adx = A.size() - 1;
+    int bdx = B.size() - 1;
+    
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    
+    while(0 <= bdx && 0 <= adx)
+    {   
+        if(A[adx] < B[bdx])
+        {
+            answer++;
+            bdx--;
+            adx--;
+        }
+        else
+        {
+            while(B[bdx] <= A[adx] && 0 <= adx && 0 <= bdx)
+            {
+                adx--;
+            }
+            
+            if(0 <= adx)
+            {
+                answer++;
+                bdx--;
+                adx--;
+            }
+        }
+    }
+    
+    return answer;
+}
