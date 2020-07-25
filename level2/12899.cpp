cb817e9d819fcef175199d20e4fb511a91c3ed2a
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-07-25 16:10:23 +0900

    Create 12899.cpp

---
+++ level2/12899.cpp
@@ -0,0 +1,60 @@
+//124 나라의 숫자
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    int total, len, x, bottom;
+    
+    if (n == 1)
+        answer = "1";
+    else if (n == 2)
+        answer = "2";
+    else if (n == 3)
+        answer = "4";
+    else
+    {
+        len = 1;
+        total = 3;
+        x = 3;
+        
+        while(total < n)
+        {
+            x *= 3;
+            total += x;
+            len++;
+        }
+        
+        bottom = total - x;
+        n -= (bottom + 1);
+        x = 1;
+        
+        for (int i = 1; i < len; ++i)
+            x *= 3;
+        
+        for (int i = 0; i < len; ++i)
+        {
+            if(n < x)
+            {
+                answer += "1";
+            }
+            else if (n < x * 2)
+            {
+                n -= x;
+                answer += "2";
+            }
+            else
+            {
+                n -= (x * 2);
+                answer += "4";
+            }
+            
+            x /= 3;
+        }
+    }
+    
+    return answer;
+}
