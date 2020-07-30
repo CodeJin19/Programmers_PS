05e29fb86c1c9ba6d4aeb56329d3b93295ce0739
CodeJin19 <48464014+CodeJin19@users.noreply.github.com>
2020-07-30 15:44:25 +0900

    Create 12985.cpp

---
+++ level2/12985.cpp
@@ -0,0 +1,25 @@
+//예상 대진표
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+    int x = a;
+    int y = b;
+    int answer = 0;
+    
+    while (x != y)
+    {
+        x++;
+        y++;
+        
+        x /= 2;
+        y /= 2;
+        
+        answer++;
+    }
+
+    return answer;
+}
